22dfc47707f395147c556c30aa8f287eea8675a5
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-19 19:02:59 +0300

    Form Component

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,10 +1,15 @@
 
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import Modal from './modal';
 import { useStoremodal } from '@/hooks/use-store-modal';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
 
 const formSchema = z.object({
@@ -13,14 +18,84 @@ const formSchema = z.object({
 
 const StoreModal = () => {
     const storeModal = useStoremodal()
+
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues:{
+        name:"",
+      }
+
+    })
+
+    const onSubmit = async(values: z.infer<typeof formSchema>)=>{
+
+      try {
+
+        setLoading(true)
+        
+        /////// api
+        
+      } catch (error) {
+        toast.error("Something went wrong")
+        
+      }
+      finally{
+        setLoading(false);
+      }
+
+    }
   return (
     <Modal
-    title='create Store'
+    title='Create Store'
     description='Add a new store'
     isOpen={storeModal.isOpen}
     onClose={storeModal.onClose}>
 
-        Form
+        <div>
+
+          <div className='space-y-6 py-2 pb-6'>
+                <div className='space-y-6'>
+                  <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                      <FormField control={form.control}
+                      name="name"
+                      render={({field})=>(
+                        <FormItem>
+                          <FormLabel>Name</FormLabel>
+                          <FormControl>
+                            <Input 
+                            disabled={loading} 
+                            placeholder='E-commerce' {...field}>
+
+                            </Input>
+                          </FormControl>
+
+
+                        </FormItem>
+                      )}/>
+
+                        <div className='pt-9 space-x-2 flex items-center justify-end w-full'>
+                          <Button  disabled={loading}  variant="secondary" onClick={storeModal.onClose}>Cancel</Button>
+                          <Button  disabled={loading}  variant="default" type='submit'>Contunie</Button>
+
+
+
+                        </div>
+
+
+
+                    </form>
+                  </Form>
+
+                  
+                </div>
+
+
+          </div>
+
+          </div>
 
     </Modal>
   )
---
+++ providers/toast-provider.tsx
@@ -0,0 +1,10 @@
+
+import { Toaster } from 'react-hot-toast';
+
+
+export const ToastProvider = ()=>{
+    return(
+        <Toaster/>
+    )
+}
+
