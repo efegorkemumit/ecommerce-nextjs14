15291d99a62a3b7a971c759866028c3d67434938
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-02-29 21:20:04 +0300

    order

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/api/:path*'],
+  publicRoutes: ['/api/:path*', "/", "/api", "/webhook" ,"/api/66758fb7-a93a-4e89-b3c3-94c34b4c0014/checkout"],
 });
  
 export const config = {
